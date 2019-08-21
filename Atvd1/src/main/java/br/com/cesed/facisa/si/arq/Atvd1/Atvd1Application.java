a9329af27ef5dfa95b770c6328f28f2d9d889b6e
Aluno <aluno@cesed.local>
2019-08-21 11:12:58 -0300

    iniciando projeto

---
+++ Atvd1/src/main/java/br/com/cesed/facisa/si/arq/Atvd1/Atvd1Application.java
@@ -0,0 +1,13 @@
+package br.com.cesed.facisa.si.arq.Atvd1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Atvd1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Atvd1Application.class, args);
+	}
+
+}
---
+++ Atvd1/src/main/java/br/com/cesed/facisa/si/arq/Atvd1/domain/Conta.java
@@ -0,0 +1,53 @@
+package br.com.cesed.facisa.si.arq.Atvd1.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class Conta implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+	
+	private int numConta;
+	
+	private double saldo;
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Conta other = (Conta) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+	
+}
